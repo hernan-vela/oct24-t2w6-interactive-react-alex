429b286d521a1d646a0225359388c376b33c597b
Alex <alex.holder@bigfootds.com>
2025-05-07 19:48:24 +1000

    feat!: fetching pokemon data to render

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,35 @@ class App extends React.Component {
     }
   }
 
-  componentDidMount(){
+  async componentDidMount(){
     console.log("Component mounted.");
+
+    let randomPokemonId = Math.floor(Math.random() * 1025) + 1;
+    console.log("Random Pokemon ID to get is: " + randomPokemonId);
+
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + randomPokemonId);
+    let data = await response.json();
+
+    console.log(data);
+
+    this.setState((previousState) => {
+      return {
+        // Guarantee that previous state is all kept
+        ...previousState,
+        // pokemonId: previousState.pokemonId,
+        // And then overwrite the specific bits that we want to update
+        pokemonName: data.name,
+        pokemonId: data.id
+      }
+    });
   }
 
   componentDidUpdate(){
     console.log(this.state);
+  }
+
+  componentWillUnmount(){
+    console.log("API stuff is all done, goodbye!");
   }
 
   render() {
